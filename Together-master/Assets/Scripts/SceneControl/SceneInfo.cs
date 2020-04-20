6fa234e7e038777239127b062ac5cd12117d8299
nagisa3113 <nagisa3113@gmail.com>
2020-04-21 00:06:39 +0800

    fixed

--- Together-master/Assets/Scripts/SceneControl/SceneInfo.cs
+++ Together-master/Assets/Scripts/SceneControl/SceneInfo.cs
@@ -22,8 +22,8 @@ public class SceneInfo : MonoBehaviour
         GameObject p2 = GameObject.Find("Player2");
         p1.transform.position = this.p1.position;
         p2.transform.position = this.p2.position;
-        p1.GetComponentInChildren<Light2D>().intensity = this.intensity;
-        p2.GetComponentInChildren<Light2D>().intensity = this.intensity;
+        // p1.GetComponentInChildren<Light2D>().intensity = this.intensity;
+        // p2.GetComponentInChildren<Light2D>().intensity = this.intensity;
 
         var targetGroup = GameObject.Find("TargetGroup").GetComponent<CinemachineTargetGroup>();
         targetGroup.m_Targets[0].target = p1.transform;
